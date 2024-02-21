cfd5210e763881de342eaa20c6c2e309556f64ab
Elena Haran <elenaharan@gmail.com>
2024-02-21 18:43:26 +0000

    Add price formatting logic

--- src/App.tsx
+++ src/App.tsx
@@ -95,12 +95,19 @@ function PropertyList({ properties }: any) {
 function PropertyCard({ property }: any) {
   if (!property) return null;
 
+  const formattedPrice = new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(property.Price);
+
   return (
     <li className="property">
       <img src={property.Image} alt="Advertised property" />
       <div>
         <p>{property.Address}</p>
-        <span className="price">{property.Price}</span>
+        <span className="price">{formattedPrice}</span>
       </div>
     </li>
   );
